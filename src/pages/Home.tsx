c1b2d76afcb957fb75f86f3b8daa75992d8eb586
Nelson Samayoa <nelson.rs.07@gmail.com>
2019-11-29 19:35:11 -0700

    Started Ionic version with Electron

---
+++ src/pages/Home.tsx
@@ -0,0 +1,51 @@
+import {
+  IonButtons,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput
+} from '@ionic/react';
+import { book, build, colorFill, grid, folderOpen } from 'ionicons/icons';
+import React from 'react';
+import './Home.css';
+
+const HomePage: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardContent>
+            <IonInput>
+            <IonIcon slot="start" color="medium" icon={folderOpen} />
+            </IonInput>
+            <IonButton color="primary">Start</IonButton>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default HomePage;
