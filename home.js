3ee0e2994b6628fb8ed98194d174beb66ff9a762
Nelson <nelson.rs.07@gmail.com>
2019-12-29 21:27:54 -0700

    Got app reporting duplicates

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 require('./components/folder-select');
 require('./components/results-display');
 
@@ -6,26 +7,54 @@ Vue.component('Home', {
   template:`
     <div>
       <folder-select
-        @folder-path="setFolderPath">
+        @folder-path="analyzeFolder">
       </folder-select>
+      <div v-if="showSpinner">Processing...</div>
       <results-display
-        v-if="paths.length > 0"
-        v-bind:items="paths">
+        v-if="results.length > 0"
+        v-bind:items="results">
       </results-display>
     </div>
     `,
     data: function() {
       return {
-        folderPath: '',
-        paths: []
+        results: [],
+        showSpinner: false
       }
     },
     methods: {
-      setFolderPath(path) {
-        this.folderPath = path;
+      analyzeFolder(path) {
+       this.showSpinner = true;
 
-        fs.readdir(this.folderPath, (err, data) => {
-          this.paths = data;
+        fs.readdir(path, (err, data) => {
+          if (err) {
+            alert(err);
+            return;
+          }
+
+          let hashes = [];
+
+          for (let path of data) {
+            if (!fs.lstatSync(path).isFile()) {
+              continue;
+            }
+
+            const content = fs.readFileSync(path);
+            const hash = crypto
+                          .createHash('md5')
+                          .update(content)
+                          .digest('hex');
+
+            const duplicate = hashes.filter(item => item.hash === hash)[0];
+
+            if (duplicate) {
+                this.results.push(`${path}, ${duplicate.path}`);
+            }
+
+            hashes.push({path, hash});
+          }
+
+          this.showSpinner = false;
         });
       }
     }
